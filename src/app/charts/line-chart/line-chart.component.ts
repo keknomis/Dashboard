cc10272cb2ad5e446620209b8a6bc1d6ae51c2fd
simon <simon.serbetar1@gmail.com>
2020-10-19 22:19:55 +0200

    line chart added

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {LINE_CHART_COLORS} from '../../shared/chart.colors';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [32, 14, 46, 23, 38, 56], label: 'Sentiment Analysis' },
+  { data: [32, 21, 46, 23, 23, 56], label: 'Forecasting' },
+  { data: [32, 14, 416, 54, 43, 56], label: 'Image Recognition' },
+];
+
+const LINE_CHART_LABELS: string[]
+  = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
+
 
 @Component({
   selector: 'app-line-chart',
@@ -9,7 +20,17 @@ export class LineChartComponent implements OnInit {
 
   constructor() { }
 
+
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+  };
+  public lineChartLegend = true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;
+
   ngOnInit(): void {
   }
-
 }
---
+++ src/app/shared/chart.colors.ts
@@ -0,0 +1,24 @@
+
+export const LINE_CHART_COLORS = [
+    {
+      backgroundColor: 'rgba(6,214,160,0.2)',
+      borderColor: 'rgba(0,200, 140,0.5)',
+      pointBackgroundColor: '#000',
+      pointHoverBackgroundColor: '#555',
+      pointHoverBorderColor: '#444'
+    },
+    {
+      backgroundColor: 'rgba(255,209,102,0.2)',
+      borderColor: 'rgba(240,180, 89,0.5)',
+      pointBackgroundColor: '#000',
+      pointHoverBackgroundColor: '#555',
+      pointHoverBorderColor: '#444'
+    },
+    {
+      backgroundColor: 'rgba(6,214,160,0.2)',
+      borderColor: 'rgba(3,64, 128,0.5)',
+      pointBackgroundColor: '#000',
+      pointHoverBackgroundColor: '#555',
+      pointHoverBorderColor: '#444'
+    },
+  ];
